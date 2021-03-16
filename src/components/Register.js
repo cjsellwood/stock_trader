a772061755c01a88eefa1d54bbfe8b7cfa09f420
cjsellwood <c.sellwood22@gmail.com>
2021-03-17 10:49:53 +1100

    Allowed registering new user

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,21 +1,79 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 const Register = (props) => {
+  const [registerForm, setRegisterForm] = useState({
+    username: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const [errorMessage, setErrorMessage] = useState("")
+
+  // Change register details when inputs change
+  const handleInput = (e) => {
+    const registerFormCopy = {
+      ...registerForm,
+      [e.target.name]: e.target.value,
+    };
+    setRegisterForm(registerFormCopy);
+  };
+
+  // Submit register form handler
+  const handleSubmission = (e) => {
+    e.preventDefault();
+    console.log(registerForm);
+    setErrorMessage("");
+
+    // Submit to backend
+    axios
+      .post("http://localhost:3000/register", {
+        ...registerForm,
+      })
+      .then((response) => {
+        console.log(response);
+        // --------- Do after successfully registering
+      })
+      .catch((error) => {
+        console.log("ERROR", error.response.data.message);
+        setErrorMessage(error.response.data.message);
+      });
+  };
+
   return (
     <div>
       <h1>Register</h1>
-      <form>
+      <h2>{errorMessage}</h2>
+      <form onSubmit={handleSubmission}>
         <div>
-          <label for="username">Username</label>
-          <input type="text" id="username" name="username" />
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            value={registerForm.username}
+            onChange={handleInput}
+          />
         </div>
         <div>
-          <label for="password">Password</label>
-          <input type="password" id="password" name="password" />
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={registerForm.password}
+            onChange={handleInput}
+          />
         </div>
         <div>
-          <label for="confirmPassword">Confirm Password</label>
-          <input type="password" id="confirmPassword" name="confirmPassword" />
+          <label htmlFor="confirmPassword">Confirm Password</label>
+          <input
+            type="password"
+            id="confirmPassword"
+            name="confirmPassword"
+            value={registerForm.confirmPassword}
+            onChange={handleInput}
+          />
         </div>
         <button type="submit" aria-label="submit">
           Submit
